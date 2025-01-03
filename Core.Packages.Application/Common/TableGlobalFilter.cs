93ede98956d0cad6661c0157c853710e9e0f04c7
Eren Akgöz <40661136+HasanErenAkgoz@users.noreply.github.com>
2025-01-03 22:51:32 +0300

    Update Ef Core implemantation

---
+++ Core.Packages.Application/Common/TableGlobalFilter.cs
@@ -0,0 +1,22 @@
+﻿using Core.Packages.Application.Interfaces;
+
+namespace Core.Packages.Domain.Common
+{
+    public class TableGlobalFilter : ITableGlobalSearchFilter
+    {
+        public TableGlobalFilter()
+        {
+            //Default Settings
+            First = 0;
+            Rows = 5;
+            SortField = "Id";
+        }
+
+        public int First { get; set; }
+        public int Rows { get; set; }
+        public string SortField { get; set; }
+        public int SortOrder { get; set; }
+        public string SearchText { get; set; }
+        public List<string> PropertyField { get; set; }
+    }
+}
